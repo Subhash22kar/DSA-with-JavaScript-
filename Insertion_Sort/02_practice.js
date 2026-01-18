8b49eecfef4aedb1cf7b877602e6462482570fe7
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-18 17:59:27 +0530

    Insertion Sort

--- Insertion_Sort/02_practice.js
+++ Insertion_Sort/02_practice.js
@@ -6,9 +6,14 @@ for(let i = 1; i<n; i++){
     let key = arr[i];
 
     let  j = i -1;
-    while(){
-        
+    while(j>=0 && arr[j]>key){
+
+        arr[j+1] = arr[j]
+        j--;
+
     }
+
+    arr[j+1] = key
 
 }
 
