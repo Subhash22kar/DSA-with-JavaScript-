cff2ae09bc2c79ccbb38ade562d7ef6fc5a0a371
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-24 22:34:02 +0530

    practice

--- GCD_HCF/01_gcb.js
+++ GCD_HCF/01_gcb.js
@@ -1,9 +1,28 @@
 
-let a = 20, b = 32;
+// let a = 20, b = 32;
 
-while(a != b){
-    if(a>b) a = a-b;
-    else b = b-a;
+// while(a != b){
+//     if(a>b) a = a-b;
+//     else b = b-a;
+// }
+
+// console.log(a);
+
+// addtional practice 
+
+let a = 32; 
+let b = 20;
+
+while( a != b){
+     if(a>b){
+         a = a-b
+
+     }
+     else{
+
+        b = b-a
+
+     }
 }
 
 console.log(a);
